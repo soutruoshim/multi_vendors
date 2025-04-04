c38a09d8e82288ba1dfb4bac0895a2fc633f3f59
srhdp <srhdp@MacBook-Pro-2.local>
2025-04-04 13:19:20 +0700

    Screen Util and GetMaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'constants/constants.dart';
+
+Widget defaultHome = const MyHomePage(title: "jsdgjhds");
 
 void main() {
   runApp(const MyApp());
@@ -10,28 +16,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return ScreenUtilInit(
+      designSize: const Size(375, 825),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Foodly',
+          // You can use the library anywhere in the app even in theme
+          theme: ThemeData(
+              scaffoldBackgroundColor: kOffWhite,
+              iconTheme: const IconThemeData(color: kDark),
+              primarySwatch: Colors.grey),
+          home: defaultHome,
+        );
+      },
     );
   }
 }
