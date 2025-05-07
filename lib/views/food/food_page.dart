7a343d502c55c2f9adf63a22694a46e2d7bbee59
srhdp <srhdp@MacBook-Pro-2.local>
2025-05-07 11:26:20 +0700

    Phone Authentication

--- lib/views/food/food_page.dart
+++ lib/views/food/food_page.dart
@@ -14,6 +14,8 @@ import 'package:multi_venors/models/foods_model.dart';
 import 'package:multi_venors/views/restaurant/restaurant_page.dart';
 import 'package:get/get.dart';
 
+import '../../common/verification_modal.dart';
+
 class FoodPage extends StatefulHookWidget {
   const FoodPage({super.key, required this.food});
 
@@ -306,71 +308,5 @@ class _FoodPageState extends State<FoodPage> {
         ],
       ),
     );
-  }
-
-  Future<dynamic> showVerificationSheet(BuildContext context) {
-    return showModalBottomSheet(
-        context: context,
-        backgroundColor: Colors.transparent,
-        showDragHandle: true,
-        builder: (BuildContext context) {
-          return Container(
-            height: 500.h,
-            width: width,
-            decoration: BoxDecoration(
-              image: const DecorationImage(
-                  image: AssetImage("assets/images/restaurant_bk.png"),
-                  fit: BoxFit.fill),
-              color: kLightWhite,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12.r),
-                topRight: Radius.circular(12.r),
-              ),
-            ),
-            child: Padding(
-              padding: EdgeInsets.all(8.h),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                  ReusableText(
-                      text: "Verify Your Phone Number",
-                      style: appStyle(18, kPrimary, FontWeight.w600)),
-                  SizedBox(
-                      height: 250.h,
-                      child: Column(
-                        children:
-                        List.generate(verificationReasons.length, (index) {
-                          return ListTile(
-                            leading: const Icon(
-                              Icons.check_circle_outline,
-                              color: kPrimary,
-                            ),
-                            title: Text(
-                              verificationReasons[index],
-                              textAlign: TextAlign.justify,
-                              style:
-                              appStyle(11, kGrayLight, FontWeight.normal),
-                            ),
-                          );
-                        }),
-                      )),
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                  CustomButton(
-                    text: "Verify  Phone Number",
-                    btnHeight: 35.h,
-                    onTap: () {
-                      //Get.to(() => const PhoneVerificationPage());
-                    },
-                  )
-                ],
-              ),
-            ),
-          );
-        });
   }
 }
